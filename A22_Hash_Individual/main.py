065921665ab63855560dc96dad35e4480d8485c6
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-09-18 14:08:20 -0600

    feat: added class activity

---
+++ A22_Hash_Individual/main.py
@@ -0,0 +1,38 @@
+a = ord("e") + \
+    ord("b") + \
+    ord(" ") + \
+    ord("n") + \
+    ord("n") + \
+    ord("i")
+
+b = ord("s") + \
+    ord("a") + \
+    ord("d") + \
+    ord("o") + \
+    ord("e") + \
+    ord("v")
+
+c = ord("t") + \
+    ord("\n") + \
+    ord("e") + \
+    ord(" ") + \
+    ord("r") + \
+    ord("o")
+
+d = ord("o") + \
+    ord("d") + \
+    ord("\n") + \
+    ord("s") + \
+    ord(" ") + \
+    ord("[")
+
+
+print(a)
+print(b)
+print(c)
+print(d, end="\n\n")
+
+print(hex(44))
+print(hex(130))
+print(hex(228))
+print(hex(203))
